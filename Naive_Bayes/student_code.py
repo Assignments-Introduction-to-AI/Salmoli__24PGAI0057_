b7cd45103e7adf81c7d62fec84b4b85a71576e39
salmoli chandra <salmoli.c24PGAI@jioinstitute.edu.in>
2023-09-09 18:24:33 +0530

    Done

--- Naive_Bayes/student_code.py
+++ Naive_Bayes/student_code.py
@@ -1,51 +1,57 @@
-import nltk
-nltk.download('punkt')
-nltk.download('wordnet')
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer, WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
+import math
 import re
 
 class Bayes_Classifier:
     def __init__(self):
-        self.vectorizer = TfidfVectorizer(ngram_range=(1, 2), stop_words='english')
-        self.clf = MultinomialNB(alpha=0.01)
-        self.ps = PorterStemmer()
-        self.wnl = WordNetLemmatizer()
+        self.classes = []
+        self.class_counts = {}
+        self.feature_counts = {}
+        self.vocabulary = set()
 
     def preprocess_text(self, text):
-        # Lower casing
-        text = text.lower()
-        
-        # Remove punctuation
-        text = re.sub(r'[^\w\s]', '', text)
-        
-        # Tokenize and stem/lemmatize text
-        tokens = word_tokenize(text)
-        stemmed_tokens = [self.ps.stem(token) for token in tokens]
-        lemmatized_tokens = [self.wnl.lemmatize(token) for token in stemmed_tokens]
-        
-        # Return preprocessed text as a string
-        return ' '.join(lemmatized_tokens)
+        # Remove non-alphabetic characters and convert to lowercase
+        text = re.sub(r'[^a-zA-Z]', ' ', text).lower()
+        # Tokenize the text
+        words = re.findall(r'\b\w+\b', text)
+        # Remove stop words
+        stop_words = set(['the', 'a', 'an', 'is', 'are', 'to', 'in', 'for', 'of'])
+        words = [word for word in words if word not in stop_words]
+
+        return words
 
     def train(self, lines):
-        texts = []
-        labels = []
         for line in lines:
-            stars, _, text = line.split('|')
-            preprocessed_text = self.preprocess_text(text)
-            texts.append(preprocessed_text)
-            labels.append(stars)
-        X = self.vectorizer.fit_transform(texts)
-        self.clf.fit(X, labels)
+            label, _, text = line.split('|')
+            words = self.preprocess_text(text)
+            if label not in self.classes:
+                self.classes.append(label)
+                self.class_counts[label] = 0
+                self.feature_counts[label] = {}
+            self.class_counts[label] += 1
+            for word in words:
+                self.vocabulary.add(word)
+                if word not in self.feature_counts[label]:
+                    self.feature_counts[label][word] = 0
+                self.feature_counts[label][word] += 1
 
     def classify(self, lines):
-        texts = []
+        predictions = []
         for line in lines:
             _, _, text = line.split('|')
-            preprocessed_text = self.preprocess_text(text)
-            texts.append(preprocessed_text)
-        X = self.vectorizer.transform(texts)
-        return self.clf.predict(X).tolist()
+            words = self.preprocess_text(text)
+            max_prob = float('-inf')
+            max_class = None
+            for c in self.classes:
+                prob_c = math.log(self.class_counts[c]) - math.log(sum(self.class_counts.values()))
+                prob_x_c = 0
+                for word in words:
+                    count_wc = self.feature_counts[c].get(word, 0) + 1
+                    count_c = sum(self.feature_counts[c].values()) + len(self.vocabulary)
+                    prob_wc = math.log(count_wc) - math.log(count_c)
+                    prob_x_c += prob_wc
+                prob_c_x = prob_c + prob_x_c
+                if prob_c_x > max_prob:
+                    max_prob = prob_c_x
+                    max_class = c
+            predictions.append(max_class)
+        return predictions
